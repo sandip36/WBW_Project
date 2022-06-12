2e223cac140bdf8f084d818829fbb1c339449f51
Sandesh Kasliwal <sandeshk@neuronimbus.com>
2022-06-12 22:02:01 +0530

    feat: add 3 times skip condn and should only show update modal once per day conditions

--- app/app.tsx
+++ app/app.tsx
@@ -13,7 +13,7 @@ import "./i18n"
 import "./utils/ignore-warnings"
 import React, { useState, useRef, useCallback } from "react"
 import { NavigationContainerRef } from "@react-navigation/native"
-import { SafeAreaView , ActivityIndicator, StyleSheet, TouchableOpacity, Linking, Modal } from "react-native"
+import { SafeAreaView , ActivityIndicator, StyleSheet } from "react-native"
 import {
     useBackButtonHandler,
     RootNavigator,
@@ -28,7 +28,6 @@ import { ThemeProvider } from "@shopify/restyle"
 import { Async } from "react-async"
 import { Box, Text } from "./components"
 import Toast from "react-native-simple-toast"
-import { checkVersion } from "react-native-check-version";
 
 
 
@@ -37,51 +36,7 @@ enableScreens()
 export const NAVIGATION_PERSISTENCE_KEY = "NAVIGATION_STATE"
 
 const styles = StyleSheet.create( {
-    button: {
-        borderRadius: 20,
-        elevation: 2,
-        padding: 10
-    },
-    buttonOpen: { 
-        backgroundColor : theme.colors.primary, 
-        borderColor : theme.colors.black, 
-        borderRadius : 5, 
-        borderWidth : 1 ,
-        marginTop : 25 
-    },
-    centeredView: {
-        alignItems: "center",
-        flex: 1,
-        justifyContent: "center",
-    },
-    modalText: {
-        fontSize: 18,
-        marginBottom: 15,
-        textAlign: "center",
-    },
-    modalView: {
-        alignItems: "center",
-        backgroundColor: theme.colors.background,
-        borderRadius: 20,
-        elevation: 5,
-        margin: 20,
-        padding: 35,
-        shadowColor: theme.colors.black,
-        shadowOffset: {
-            width: 0,
-            height: 2
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 4,
-        width: "80%"
-    },
-    rootContainer: { backgroundColor: theme.colors.primary, flex: 1 },
-    textStyle: {
-        color: theme.colors.background,
-        fontSize: 18,
-        fontWeight: "bold",
-        textAlign: "center",
-    }
+    rootContainer: { backgroundColor: theme.colors.primary, flex: 1 }
 } )
 
 /**
@@ -90,57 +45,21 @@ const styles = StyleSheet.create( {
 function App () {
     const navigationRef = useRef<NavigationContainerRef>()
     const [ rootStore, setRootStore ] = useState<RootStore | undefined>( undefined )
-    const [ shouldUpdateApplication, setShouldUpdateApplication ] = useState<boolean>( false )
 
     setRootNavigation( navigationRef )
     useBackButtonHandler( navigationRef, canExit )
-
-
-
-
-    
+ 
     const bootstrapApplication = useCallback( async ( ) => {
 
         try {
 
             // await codePush.sync()
             const rootStore = await setupRootStore()
-            const version = await checkVersion();
-            console.log( "Got version info:", version );
-
-            if ( !version.needsUpdate ) {
-                console.log( `App has a ${version.updateType} update pending.` );
-                setShouldUpdateApplication( true )
-            }
             setRootStore( rootStore )
         } catch ( error ) {
             Toast.showWithGravity( error.message || 'Something went wrong while setting up root store', Toast.LONG, Toast.CENTER )
         }
     }, [] )
-
-    const updateDialogbox = ( ) => {
-        return(
-            <Box style={styles.centeredView}>
-                <Modal
-                    animationType="slide"
-                    transparent={true}
-                    visible={true}
-                >
-                    <Box style={styles.centeredView}>
-                        <Box style={styles.modalView}>
-                            <Text style={styles.modalText}>A new update is available. Please download the latest version from Playstore.</Text>
-                            <TouchableOpacity
-                                style={[ styles.button, styles.buttonOpen ]}
-                                onPress = {() => Linking.openURL( "market://details?id=com.wbw" )}
-                            >
-                                <Text style={styles.textStyle}>Update Application</Text>
-                            </TouchableOpacity>
-                        </Box>
-                    </Box>
-                </Modal>
-            </Box>
-        )
-    }
 
     // otherwise, we're ready to render the app
     return (
@@ -160,20 +79,15 @@ function App () {
                 ) }
             </Async.Rejected>
             <Async.Resolved>
-                {
-                    shouldUpdateApplication
-                        ? updateDialogbox()
-                        : 
-                        <ThemeProvider {...{ theme }}>
-                            <RootStoreProvider value={rootStore}>
-                                <SafeAreaView style={styles.rootContainer}>
-                                    <RootNavigator
-                                        ref={navigationRef}
-                                    />
-                                </SafeAreaView>
-                            </RootStoreProvider>
-                        </ThemeProvider>
-                }
+                <ThemeProvider {...{ theme }}>
+                    <RootStoreProvider value={rootStore}>
+                        <SafeAreaView style={styles.rootContainer}>
+                            <RootNavigator
+                                ref={navigationRef}
+                            />
+                        </SafeAreaView>
+                    </RootStoreProvider>
+                </ThemeProvider>
             </Async.Resolved>
         </Async>
     )
