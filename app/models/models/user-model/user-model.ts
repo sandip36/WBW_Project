d5ca8ac7607d7edca45dcc4c6d3baef876f72e17
sandipjadhav <sandipjadhav@consultbop.com>
2022-06-12 23:05:13 +0530

    Android app update version implimented

--- app/models/models/user-model/user-model.ts
+++ app/models/models/user-model/user-model.ts
@@ -1,4 +1,4 @@
-import { Instance, SnapshotOut, types } from "mobx-state-tree"
+import { flow, Instance, SnapshotOut, types } from "mobx-state-tree"
 import { createModel } from '../../factories/model.factory'
 
 
@@ -18,11 +18,37 @@ export const UserModel = createModel( {
     CompanyID: types.maybeNull( types.string ),
     CompanyName: types.maybeNull( types.string ),
     LevelName: types.maybeNull( types.string ),
+    skipCount: types.optional( types.number, 0 ),
+    skippedDate: types.maybe( types.Date )
 } )
     .named( 'UserModel' )
+    .views( self => ( {
+        shouldShowUpdateModal ( ) {
+            const currentDate = new Date()
+            return self.skippedDate?.toDateString() !== currentDate.toDateString()
+        },
+    } ) )
+    .actions( self => {
+        const updateSkipCount = flow( function * ( count = 1 ) {
+            self.skipCount += count
+        } )
+
+        const setSkippedDate = flow( function * ( ) {
+            self.skippedDate = new Date()
+        } )
+
+        const resetSkipCount = flow( function * ( ) {
+            self.skipCount = 0
+        } )
+
+        return {
+            updateSkipCount,
+            setSkippedDate,
+            resetSkipCount
+        }
+    } )
 
 type UserType = Instance<typeof UserModel>
 export interface IUser extends UserType {}
 type UserSnapshotType = SnapshotOut<typeof UserModel>
 export interface IUserSnapshot extends UserSnapshotType {}
-
--- app/screens/dashboard-home/dashboard-home-screen.tsx
+++ app/screens/dashboard-home/dashboard-home-screen.tsx
@@ -1,22 +1,80 @@
-import { Box, Header, Text } from "components"
+import { Box, Header, Text, TouchableBox } from "components"
 import { DashboardCard } from "components/dashboard"
 import { useStores } from "models"
-import React, { useCallback } from "react"
+import React, { useCallback, useState } from "react"
 import { Async } from "react-async"
-import { ActivityIndicator, Alert, FlatList } from "react-native"
+import { ActivityIndicator, Alert, FlatList, Linking, Modal, StyleSheet, TouchableOpacity } from "react-native"
+import { checkVersion } from "react-native-check-version"
 import { theme } from "theme"
 
 export type DashboardHomeScreenProps = {
 
 }
 
+const styles = StyleSheet.create( {
+    button: {
+        borderRadius: 20,
+        elevation: 2,
+        padding: 10
+    },
+    buttonOpen: { 
+        backgroundColor : theme.colors.primary, 
+        borderColor : theme.colors.black, 
+        borderRadius : 5, 
+        borderWidth : 1 ,
+        marginTop : 25 
+    },
+    centeredView: {
+        alignItems: "center",
+        flex: 1,
+        justifyContent: "center",
+    },
+    modalText: {
+        fontSize: 18,
+        marginBottom: 15,
+        textAlign: "center",
+    },
+    modalView: {
+        alignItems: "center",
+        backgroundColor: theme.colors.background,
+        borderRadius: 20,
+        elevation: 5,
+        margin: 20,
+        padding: 35,
+        shadowColor: theme.colors.black,
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        width: "80%"
+    },
+    textStyle: {
+        color: theme.colors.background,
+        fontSize: 18,
+        fontWeight: "bold",
+        textAlign: "center",
+    }
+} )
+
 export const DashboardHomeScreen: React.FunctionComponent<DashboardHomeScreenProps> = ( ) => {
     const { DashboardStore, AuthStore, ObservationStore, AuditStore } = useStores()
+    const [ shouldUpdateApplication, setShouldUpdateApplication ] = useState<boolean>( false )
+
 
     const fetchDashboard = useCallback( async () => {
-        await ObservationStore._clear()
-        await AuditStore.resetStore()
-        await DashboardStore.fetch()
+        const version = await checkVersion();
+        console.log( "Got version info:", version  );
+
+        if ( version.needsUpdate && AuthStore.user.skipCount < 3 && AuthStore.user.shouldShowUpdateModal() ) {
+            console.log( `App has a ${version.updateType} update pending.` );
+            setShouldUpdateApplication( true )
+        }else{
+            await ObservationStore._clear()
+            await AuditStore.resetStore()
+            await DashboardStore.fetch()
+        }
     }, [] )
 
     const renderItem = ( { item } ) => {
@@ -43,6 +101,41 @@ export const DashboardHomeScreen: React.FunctionComponent<DashboardHomeScreenPro
         return true
     }
 
+    const skipUpdate = async ( ) => {
+        await AuthStore.user.updateSkipCount()
+        await AuthStore.user.setSkippedDate()
+        setShouldUpdateApplication( false )
+    }
+
+    const updateDialogbox = ( ) => {
+        return(
+            <Box style={styles.centeredView}>
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={true}
+                >
+                    <Box style={styles.centeredView}>
+                        <Box style={styles.modalView}>
+                            <Text style={styles.modalText}>Update is available. Please download the latest version.</Text>
+                            <TouchableOpacity
+                                style={[ styles.button, styles.buttonOpen ]}
+                                onPress = {() => Linking.openURL( "market://details?id=com.wbw" )}
+                            >
+                                <Text style={styles.textStyle}>Update</Text>
+                            </TouchableOpacity>
+                            <TouchableBox justifyContent="center" marginTop="large" alignItems="center" onPress={skipUpdate}>
+                                <Text textAlign="center" fontSize={theme.spacing.regular * 1.2}>
+                                Not Now
+                                </Text>
+                            </TouchableBox>
+                        </Box>
+                    </Box>
+                </Modal>
+            </Box>
+        )
+    }
+
     return (
         <Box flex={1}>
             <Async promiseFn={fetchDashboard}>
@@ -61,20 +154,24 @@ export const DashboardHomeScreen: React.FunctionComponent<DashboardHomeScreenPro
                     ) }
                 </Async.Rejected>
                 <Async.Resolved>
-                    <Box flex={1}>
-                        <Header 
-                            title={AuthStore.user?.CompanyName}
-                            rightComponent={{ icon: 'logout', color: '#fff', type: 'material', onPress: onRightIconPress, style: { marginHorizontal: theme.spacing.small } }}
-                        />
-                        <Box mt="small">
-                            <FlatList 
-                                data={DashboardStore.sortDashboardByPageOrder}
-                                renderItem={renderItem}
-                                keyExtractor={( item, index ) => item.id }
-                                contentContainerStyle={{ paddingBottom: 100 }}
-                            />
-                        </Box>     
-                    </Box>
+                    {
+                        shouldUpdateApplication
+                            ? updateDialogbox()
+                            :  <Box flex={1}>
+                                <Header 
+                                    title={AuthStore.user?.CompanyName}
+                                    rightComponent={{ icon: 'logout', color: '#fff', type: 'material', onPress: onRightIconPress, style: { marginHorizontal: theme.spacing.small } }}
+                                />
+                                <Box mt="small">
+                                    <FlatList 
+                                        data={DashboardStore.sortDashboardByPageOrder}
+                                        renderItem={renderItem}
+                                        keyExtractor={( item, index ) => item.id }
+                                        contentContainerStyle={{ paddingBottom: 100 }}
+                                    />
+                                </Box>     
+                            </Box>
+                    }
                 </Async.Resolved>
             </Async>
         </Box>
