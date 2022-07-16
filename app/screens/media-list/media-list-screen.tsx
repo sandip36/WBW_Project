538d3f0995d5ca699d4b9c4b74f2325ab046525a
Sandesh Kasliwal <sandeshk@neuronimbus.com>
2022-07-16 21:52:54 +0530

    feat: add sample UI for media list screen

--- app/screens/media-list/media-list-screen.tsx
+++ app/screens/media-list/media-list-screen.tsx
@@ -1,67 +1,37 @@
 import { useNavigation } from "@react-navigation/native"
-import { Box, Text } from "components"
+import { Box, Text, TouchableBox } from "components"
 import { FormHeader } from "components/core/header/form-header"
 import React, { useCallback, useState } from "react"
-import { Dimensions, FlatList, Image, ActivityIndicator } from "react-native"
+import { Dimensions, FlatList, Image, ActivityIndicator, Linking } from "react-native"
 import { Avatar, ListItem } from "react-native-elements"
 import Video from "react-native-video"
 import Async from "react-async"
 import { useStores } from "models"
+import InAppBrowser from "react-native-inappbrowser-reborn"
 
 export type MediaListScreenProps = {
 
 }
 
 
-const postsList = {
-    "posts": [
-        {
-            "id": 1,
-            "username": "Sandip",
-            "title": "Uddhav Thackeray Resigns",
-            "subtitle": "Maharashtra Government",
-            "logo": "https://cdn1.iconfinder.com/data/icons/avatars-55/100/avatar_profile_user_music_headphones_shirt_cool-512.png",
-            "description": "lorem ipsum is the best",
-            "media": [
-                {
-                    "mediaId": 101,
-                    "postId": 1,
-                    "type": "image",
-                    "uri": "https://th.bing.com/th/id/OIP.1U3zHtP_FzMHeyF5zTtHDQHaDt?pid=ImgDet&rs=1"
-                },
-                {
-                    "mediaId": 102,
-                    "postId": 1,
-                    "type": "video",
-                    "uri": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-                }
-            ]
-        },
-        {
-            "id": 2,
-            "username": "Sandesh",
-            "title": "Indian Cricket in right hands",
-            "subtitle": "Rahul-Rohit leads team",
-            "logo": "https://cdn1.iconfinder.com/data/icons/avatars-55/100/avatar_profile_user_music_headphones_shirt_cool-512.png",
-            "description": "lorem ipsum is the best",
-            "media": [
-                {
-                    "mediaId": 103,
-                    "postId": 2,
-                    "type": "image",
-                    "uri": "https://th.bing.com/th/id/OIP.1U3zHtP_FzMHeyF5zTtHDQHaDt?pid=ImgDet&rs=1"
-                },
-                {
-                    "mediaId": 104,
-                    "postId": 2,
-                    "type": "video",
-                    "uri": "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-                }
-            ]
-
-        }
-    ]
-}
+const mediaList = [
+    {
+        BulletinID:"82dace18-cf67-4fc3-87d4-8e1fe04d60fd",
+        Title:"TestBulletine",
+        Description:"Loremipsumdolorsitamet,consecteturadipiscingelit,seddoeiusmodtemporincididuntutlaboreetdoloremagnaaliqua.Utenimadminimveniam,quisnostrudexercitationullamcolaborisnisiutaliquipexeacommodoconsequat.Duisauteiruredolorinreprehenderitinvoluptatevelitessecillumdoloreeufugiatnullapariatur.Excepteursintoccaecatcupidatatnonproident,suntinculpaquiofficiadeseruntmollitanimidestlaborum",
+        VideoPath:"http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
+        ImagePath:"https://cdn1.iconfinder.com/data/icons/avatars-55/100/avatar_profile_user_music_headphones_shirt_cool-512.png",
+        PlayVideoOnDelivery:"True",
+        Link1Name:"Register",
+        Link1:"https://demo.wisebusinessware.com/",
+        Link2Name:"",
+        Link2:"",
+        CreatedOn:"7/6/202212:00:00AM",
+        CreatedByName:"admin,wardmfg",
+        FirstName:"wardmfg",
+        LastName:"admin"
+    }
+]
 
 
 export const MediaListScreen: React.FunctionComponent<MediaListScreenProps> = ( ) => {
@@ -128,23 +98,50 @@ export const MediaListScreen: React.FunctionComponent<MediaListScreenProps> = ( 
         )
     }
 
+    const openInAppBrowser = async ( link ) => {
+        try {
+            // const token = await AsyncStorage.getItem( 'Token' )
+            if( link ) {
+                if ( await InAppBrowser.isAvailable() ) {
+                    const result = await InAppBrowser.open( link )
+                }
+                else Linking.openURL( link )
+            }
+        } catch ( error ) {
+            await InAppBrowser.close()
+            if ( await InAppBrowser.isAvailable() ) {
+                const result = await InAppBrowser.open( link )
+            }
+            else Linking.openURL( link )
+        }
+    }
+
     const renderItem = ( { item, index } ) => {
         return (
-            <Box flex={1}>
+            <Box flex={1} mx="medium">
                 <ListItem>
-                    <Avatar rounded size="small" source={{ uri: item.logo }} />
+                    <Avatar rounded size="small" source={{ uri: item.ImagePath }} />
                     <ListItem.Content>
-                        <ListItem.Title>{item.title}</ListItem.Title>
-                        <ListItem.Subtitle>{item.subtitle}</ListItem.Subtitle>
+                        <ListItem.Title>{item.FirstName} {item.LastName}</ListItem.Title>
+                        <ListItem.Subtitle>{item.CreatedOn}</ListItem.Subtitle>
                     </ListItem.Content>
                 </ListItem>
-                <FlatList 
-                    data={item.media}
-                    renderItem={renderPostMedia}
-                    horizontal
-                    keyExtractor={( item ) => String( item.mediaId )}
-                    ItemSeparatorComponent={ItemSeparatorComponent}
-                />
+                <Box flex={0.35}>
+                    <Video 
+                        source={{ uri: item.VideoPath }}   // Can be a URL or a local file.
+                    />
+                </Box>
+                <Box>
+                    <Text variant="body">{item.Description}</Text>
+                </Box>
+                <Box>
+                    <TouchableBox onPress={ ( ) => openInAppBrowser( item.Link1 ) }>
+                        <Text color="primary">{item.Link1Name}</Text>
+                    </TouchableBox>     
+                    <TouchableBox onPress={ ( ) => openInAppBrowser( item?.Link2 ) }>
+                        <Text color="primary">{item?.Link2Name}</Text>
+                    </TouchableBox>     
+                </Box>
             </Box>
         )
     }
@@ -184,8 +181,12 @@ export const MediaListScreen: React.FunctionComponent<MediaListScreenProps> = ( 
                         title="Media"
                         navigation={navigation}
                     />
-                    <Box>
-                        <Text>Inside Media Screen</Text>
+                    <Box flex={1}>
+                        <FlatList 
+                            data={mediaList}
+                            renderItem={renderItem}
+                            keyExtractor={( item )=> String( item.BulletinID )}                
+                        />
                     </Box>
                 </Box>
             </Async.Resolved>
